dcec572f3a78170e547742b475e8d518c415dad3
Duncan Bain <duncan@duncanbain.dev>
2023-09-25 17:36:17 +0100

    basic auth

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,18 @@
-import './reset.css'
-import './styles.scss'
+import "./reset.css";
+import "./styles.scss";
 
-import { Inter } from 'next/font/google'
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>{children}</body>
+      </UserProvider>
     </html>
-  )
+  );
 }
